5e91245d405b342894f43e3123f592bc7387b338
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-02-09 23:55:01 -0500

    updating to v2

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Introduction to Formal Systems",
+        "excerpt":"","categories": ["provable-software"],
+        "tags": [],
+        "url": "http://localhost:4000/provable-software/2020/02/08/intro.html"
+      }]
